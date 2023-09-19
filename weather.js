4e3a954c36149c381f4daa309411811db73b2bc5
UTHMN <Kingvcheese@gmail.com>
2023-09-19 19:24:57 +0100

    Hehe

--- weather.js
+++ weather.js
@@ -1,17 +1,42 @@
-const apiKey = "af1c47118ac9f9defffe6d12437bc3a0";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiKey = "d05209b7b4075f46ee62255e9a0bdb95";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + '&appid=${apiKey}');
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const WeatherIcon = document.querySelector(".weather-icon");
+
+console.log("hehehe here is my very secret api key: " + apiKey)
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + '&appid=${apiKey}');
     var data = await response.json()
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidty").innerHTML = data.main.humidty + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h" ; 
+
+    if(data.weather[0].main == "Clouds"){
+        WeatherIcon.src = "weatherimg/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        WeatherIcon.src = "weatherimg/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        WeatherIcon.src = "weatherimg/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        WeatherIcon.src = "weatherimg/mist.png";
+    }
+
+    document.ATTRIBUTE_NODE.querySelector(".weather").style.display = "block";
+
 }
 
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
 
 checkWeather();
