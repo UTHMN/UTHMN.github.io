59480e131a902bdaa6f70dd60049d1ce4288e51a
UTHMN <Kingvcheese@gmail.com>
2023-10-10 13:57:40 +0100

    Removed Sentry

--- node_modules/@sentry/replay/types/coreHandlers/performanceObserver.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ReplayContainer } from '../types';
-/**
- * Sets up a PerformanceObserver to listen to all performance entry types.
- */
-export declare function setupPerformanceObserver(replay: ReplayContainer): PerformanceObserver;
-//# sourceMappingURL=performanceObserver.d.ts.map
