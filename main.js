0eeec92b6f184d27ce5b30bb5e29fa64770f1be8
UTHMN <Kingvcheese@gmail.com>
2023-10-10 11:41:55 +0100

    Added sentry support

---
+++ main.js
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://da9cc095e159db0a8af8691a8dffc149@o4505987316056064.ingest.sentry.io/4505987328442368",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
